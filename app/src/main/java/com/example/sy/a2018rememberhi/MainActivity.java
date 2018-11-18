4f70da1f7f61bdf0df04e870cf6c4a71021668b5
HongEunBeen <vvvv980@gmail.com>
2018-11-18 15:23:38 +0900

    DB 연결

--- app/src/main/java/com/example/sy/a2018rememberhi/MainActivity.java
+++ app/src/main/java/com/example/sy/a2018rememberhi/MainActivity.java
@@ -3,10 +3,17 @@ package com.example.sy.a2018rememberhi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference databaseReference = database.getReference("message");
+        databaseReference.setValue("Hello, World!");
     }
 }
