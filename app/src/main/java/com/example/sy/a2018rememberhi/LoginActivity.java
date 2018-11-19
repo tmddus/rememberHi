a491c2a4763cfd1580eaa4fb7bb5b0744d7085ce
unknown <wnzk413@gmail.com>
2018-11-19 19:01:30 +0900

    쓸데없는 커밋,,

--- app/src/main/java/com/example/sy/a2018rememberhi/LoginActivity.java
+++ app/src/main/java/com/example/sy/a2018rememberhi/LoginActivity.java
@@ -2,6 +2,9 @@ package com.example.sy.a2018rememberhi;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        EditText InputId = findViewById(R.id.inputId);
+        EditText InputPwd = findViewById(R.id.inputPwd);
+        Button loginBtn = findViewById(R.id.loginbtn);
+
+
+
+
+
     }
 }
