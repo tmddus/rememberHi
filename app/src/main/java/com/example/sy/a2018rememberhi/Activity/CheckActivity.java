3c963a3d174ce84bebec7ddc321b8fe25387874f
unknown <wnzk413@gmail.com>
2018-11-21 01:37:21 +0900

    선택된 체크리스트 개수 구하기

--- app/src/main/java/com/example/sy/a2018rememberhi/Activity/CheckActivity.java
+++ app/src/main/java/com/example/sy/a2018rememberhi/Activity/CheckActivity.java
@@ -3,6 +3,7 @@ package com.example.sy.a2018rememberhi.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.SparseBooleanArray;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
@@ -17,6 +18,7 @@ public class CheckActivity extends AppCompatActivity {
     checkListViewAdapter adapter;
     ArrayList<String> checkListText;
     Button okBtn;
+    int Checks=0; // 체크된 개수 저장하는 변수
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +42,12 @@ public class CheckActivity extends AppCompatActivity {
         okBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                SparseBooleanArray checkedItems = checkListView.getCheckedItemPositions();
+                for (int i = adapter.getCount() - 1; i >= 0; i--) {
+                    if (checkedItems.get(i))
+                        Checks++;
+                }
+
                 Intent intent = new Intent(CheckActivity.this, ProfileActivity.class);
                 startActivity(intent);
                 finish();
