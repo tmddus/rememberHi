b8ac37844614321400a8c7ab0813ad7f8505c3c2
codnjs <ecwecw1206@naver.com>
2018-11-22 01:58:52 +0900

    중복버튼, 회원가입 버튼, 스피너어뎁터

--- app/src/main/java/com/example/sy/a2018rememberhi/Activity/TodayendActivity.java
+++ app/src/main/java/com/example/sy/a2018rememberhi/Activity/TodayendActivity.java
@@ -3,10 +3,15 @@ package com.example.sy.a2018rememberhi.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 
 import com.example.sy.a2018rememberhi.R;
+
+import java.util.ArrayList;
 
 public class TodayendActivity extends AppCompatActivity {
 
@@ -14,6 +19,8 @@ public class TodayendActivity extends AppCompatActivity {
     Button postBtn;
     String TodayWeather;
     String TodayFeeling;
+    Spinner spinnerWeather, spinnerTension;
+    ArrayAdapter spinnerWeatherAdap, spinnerTensionAdap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +29,43 @@ public class TodayendActivity extends AppCompatActivity {
         postBtn = findViewById(R.id.postBtn);
         todayEtc = findViewById(R.id.todayEtc);
 
+        final ArrayList<String> weather = new ArrayList<>();
+        final ArrayList<String> tension = new ArrayList<>();
+
+        spinnerWeather = findViewById(R.id.spinner_weather);
+        spinnerTension = findViewById(R.id.spinner_tension);
+
+        spinnerWeatherAdap = new ArrayAdapter(this, R.layout.support_simple_spinner_dropdown_item, weather);
+        spinnerWeather.setAdapter(spinnerWeatherAdap);
+
+        spinnerTensionAdap = new ArrayAdapter(this, R.layout.support_simple_spinner_dropdown_item, tension);
+        spinnerTension.setAdapter(spinnerTensionAdap);
+
+        final Spinner spinner = (Spinner) findViewById(R.id.age);
+
+        spinnerWeather.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                TodayWeather = (String)spinnerWeather.getItemAtPosition(position);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        spinnerTension.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                TodayFeeling = (String)spinnerTension.getItemAtPosition(position);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
 
         postBtn.setOnClickListener(new View.OnClickListener() {
             @Override
